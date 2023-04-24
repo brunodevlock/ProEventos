0be5487c826bcc2144d9a50fb2eaef66dfb2f61c
unknown <brunoscatena.9@gmail.com>
2023-04-24 16:30:19 -0300

    Migrations - DataBase Sqlite - Controller

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,48 +13,24 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-       
-        public EventoController()
+
+        private DataContext _context { get; }
+        public EventoController(DataContext context)
         {
+            _context = context;
            
         } 
-
-        public IEnumerable<Evento> _evento = new Evento[] {
-
-            new Evento() {
-                EventoId = 1,
-                Tema = "Aula 1",
-                Local = "online",
-                Lote = "1",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString(),
-                ImagemURL = "foto.png"
-            },
-
-            new Evento() {
-                EventoId = 2,
-                Tema = "Aula 2",
-                Local = "online",
-                Lote = "1",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString(),
-                ImagemURL = "foto2.png"
-            }
-
-
-        };
-
-        
+               
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
         public IEnumerable<Evento> GetByid(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.Where(evento => evento.EventoId == id);
         }
 
     }
